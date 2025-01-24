6cf972a7035ec7c5a792104034edde68b1d59af2
NasyaZafira <nasya.zafira16@gmail.com>
2025-01-24 17:14:53 +0700

    Initial Commit

---
+++ tc/src/main/java/com/restful/tc/TcApplication.java
@@ -0,0 +1,17 @@
+package com.restful.tc;
+
+// Ensure the correct JDK is configured in your development environment.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcApplication.class, args);
+
+
+	}
+
+}
