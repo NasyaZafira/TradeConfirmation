52e8bce209dfff2a88763c13c62b337112909684
NasyaZafira <nasya.zafira16@gmail.com>
2025-02-06 10:07:12 +0700

    zip pdf logic new

--- tc/src/main/java/com/restful/tc/repository/InvRepository.java
+++ tc/src/main/java/com/restful/tc/repository/InvRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InvRepository extends JpaRepository<Invoice, Long> {
-    List<Invoice> findAll();
+    //    List<Invoice> findAll();
+    List<Invoice> findFirst10ByOrderByNoInvAsc();
 }
